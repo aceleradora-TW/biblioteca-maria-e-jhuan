486b2ccc8aa3a4a2c030be99b2b94249dba05230
eduardaschuler <dudaschuler2011@gmail.com>
2020-06-10 10:15:58 -0300

    01/@EduardaSchuler, @Jhuan0999 :apresenta mensagem de boas vindas

--- src/main/java/com/aceleradora/biblioteca/Main.java
+++ src/main/java/com/aceleradora/biblioteca/Main.java
@@ -3,6 +3,6 @@ package com.aceleradora.biblioteca;
 public class Main {
 
     public static void main(String[] args) {
-
+        System.out.println("Bem vindas e bem vindos a nossa Biblioteca");
     }
 }
