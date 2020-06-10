21ef50b04fdf408cb583542d198a0f1f83a3ad54
eduardaschuler <dudaschuler2011@gmail.com>
2020-06-10 17:54:41 -0300

    01- @eduardaschuler @jhuan0999: ajustes

--- src/main/java/com/aceleradora/biblioteca/Main.java
+++ src/main/java/com/aceleradora/biblioteca/Main.java
@@ -6,19 +6,17 @@ public class Main {
         String[] autores = new String[3];
         titulos[0] = "Program development in java";
         autores[0] = "Barbara Liskov";
-        titulos[1] = "use a cabeca: padroes de projeto";
-        autores[1] = "elisabeth freeman";
-        autores[2] = "kathy sierra";
+        titulos[1] = "Use a cabeça: padrões de projeto";
+        autores[1] = "Elisabeth Freeman";
+        autores[2] = "Kathy Sierra";
 
-        System.out.println("Bem vindas e bem vindos a nossa Biblioteca");
-        System.out.println("Temos os seguintes livros disponiveis:");
-        System.out.println("");
-        System.out.println("Livro 1:");
+        System.out.println("----Bem vindas e bem vindos a nossa Biblioteca----");
+        System.out.println("-----Temos os seguintes livros disponiveis:-------" + "\n");
+        System.out.println("-------Livro 1:------");
         System.out.println(titulos[0]);
-        System.out.println(autores[0]);
-        System.out.println("");
-        System.out.println("livro 2:");
+        System.out.println("De: " + autores[0] + "\n");
+        System.out.println("-------Livro 2:-------");
         System.out.println(titulos[1]);
-        System.out.println(autores[1] + " e " + autores[2]);
+        System.out.println("De: " + autores[1] + " e " + autores[2]);
     }
 }
